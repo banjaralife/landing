2d31e007fa30af27b760068756462a96ff172984
keogami <41939011+keogami@users.noreply.github.com>
2025-11-02 04:28:06 +0530

    feat: add posthog reverse proxy

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -8,7 +8,8 @@ export function PHProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     if (typeof window !== 'undefined') {
       posthog.init('phc_r6EzNWdnoa9pwsogHv3RarzvSZbghC4p1A3FLyvsv8g', {
-        api_host: 'https://eu.i.posthog.com',
+        api_host: '/ingest',
+        ui_host: 'https://eu.posthog.com',
         capture_pageview: true,
         capture_pageleave: true,
       });
