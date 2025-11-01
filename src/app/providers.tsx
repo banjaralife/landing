4942330f2766f2211ca450241c193d3d635c414a
keogami <41939011+keogami@users.noreply.github.com>
2025-11-02 04:39:40 +0530

    feat: conditionally load analytics

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 
 export function PHProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_ENABLE_ANALYTICS === 'true') {
       posthog.init('phc_r6EzNWdnoa9pwsogHv3RarzvSZbghC4p1A3FLyvsv8g', {
         api_host: '/ingest',
         ui_host: 'https://eu.posthog.com',
